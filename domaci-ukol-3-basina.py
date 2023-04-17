0cf9356a275b9768d0bf929a04abc98722710c83
kateryna-basina <40661251+kateryna-basina@users.noreply.github.com>
2023-04-17 13:29:54 +0200

    Update domaci-ukol-3-basina.py

--- domaci-ukol-3-basina.py
+++ domaci-ukol-3-basina.py
@@ -1,17 +1,23 @@
 import json
+
+#nacteme soubor a prevedeme ho na slovnik v pytonu
+
 with open ('body.json', encoding='utf-8') as file:
     soubor = json.load(file)
 
 print(soubor)
 
-prospeli = {}
-for student, znamka in soubor.items():
+prospeli = {}  #vytvarime novy prazdny slovnik, kde budeme zobrazovat znamky "Pass" nebo "Fail"
+
+for student, znamka in soubor.items(): #projdeme ten cely soubor cyklusom "for"
     if znamka >= 60:
         prospeli[student] = "Pass"
     else: 
-        prospeli[student] = "Fail"
+        prospeli[student] = "Fail" #estli znamka je vice nebo ravna 60, student bude mit "Pass", jinak bude mit "Fail"
+        
+#ulozime nove znamky jako json soubor
 
-with open ('prospeli.json', 'w') as file:
+with open ('prospeli.json', 'w') as file: 
     json.dump(prospeli, file)
 
-print (prospeli)
+print (prospeli) #zobrazime vysledek
